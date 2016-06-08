c01b614c9366ce4639282c56e1baffc15621159d
Minsheng <shuangrain@hotmail.com>
2016-06-08 10:33:25 +0800

    Initi

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-# WebApplication_Upload
+# 使用 HttpPostedFileBase 檔案上傳
 ASP.NET MVC5 Upload Test
+
+教學：<a href="https://blog.sofast.info/2016/06/c-asp-net-mvc5-use-the-httppostedfilebase-file-upload/">https://blog.sofast.info/2016/06/c-asp-net-mvc5-use-the-httppostedfilebase-file-upload/</a>
---
+++ WebApplication_Upload/WebApplication_Upload/Controllers/HomeController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication_Upload.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult Upload()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Upload(HttpPostedFileBase file)
+        {
+            if (file != null && file.ContentLength > 0)
+            {
+                var fileName = Path.GetFileName(file.FileName);
+                var path = Server.MapPath("~/App_Data/FileUploads");
+                //若該資料夾不存在，則新增一個
+                if (!Directory.Exists(path))
+                {
+                    Directory.CreateDirectory(path);
+                }
+                path = Path.Combine(path, fileName);
+                file.SaveAs(path);
+            }
+            return RedirectToAction("Upload");
+        }
+    }
+}
